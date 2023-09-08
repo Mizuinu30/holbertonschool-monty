6f316b45feeb071000c0c62ed99d85c9ef11d51e
Mizuinu30 <juansilva.dvm@gmail.com>
2023-09-08 13:02:03 +0000

    fixed betty and separated the push and pall function into two dif files

---
+++ push.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * push - pushes new node to the end of the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: the number of a line of the file
+ *
+ * Return: void
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node;
+	char *numbers;
+
+	numbers = strtok(NULL, DELIMS);
+	if (numbers == NULL)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	node->n = atoi(numbers);
+	node->prev = NULL;
+	node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = node;
+
+	*stack = node;
+}
+
