2e7c75b0082e26928b94667ff630c72f021b5704
Mizuinu30 <juansilva.dvm@gmail.com>
2023-09-09 19:06:16 +0000

    commands added, also fix sprint to sprintf

--- 3-swap.c
+++ 3-swap.c
@@ -12,7 +12,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (var.stack_len < 2)
 	{
-		sprint(STDOUT_FILENO, "L%u: can't swap, stack too short\n", line_number);
+		sprintf(STDOUT_FILENO, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if (var.stack_len == 2)
--- monty.h
+++ monty.h
@@ -52,5 +52,5 @@ void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
-
+void global_free(void);
 #endif
