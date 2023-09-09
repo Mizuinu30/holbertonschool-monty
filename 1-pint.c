ee2bb5a4fe5de147bf2de152543e68e70c4bb8c4
Scopecr <wjrcarrion@gmail.com>
2023-09-09 19:21:13 +0000

    changes to pint remove variable var

--- 1-pint.c
+++ 1-pint.c
@@ -1,21 +1,24 @@
 #include "monty.h"
 /**
- * m_pint - prints the value of the top of the stack
+ * pint - prints the value of the top of the stack
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
  */
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-	stack_t var;
+	stack_t *temp = NULL;
 
-	stack_t *head = *stack;
-
-	if (var.stack_len == 0)
+	if (stack == NULL || *stack == NULL)
 	{
-		sprintf(STDOUT_FILENO, "L%u: can't print, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
+		pint(line_number);
+		return;
 	}
-	printf("%d\n", head->n);
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+	
+	printf("%d", temp->n);
+	printf("\n");
 }
 
