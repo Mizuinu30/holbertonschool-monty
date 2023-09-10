1d8b2279f7edcc9191307a335ae57ca3c61471d2
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-09-10 16:12:56 -0400

    Changes to main file added content pointer size and line_ number variable

--- main.c
+++ main.c
@@ -7,12 +7,17 @@ stack_t **global_head;
  * @argc: how many arguments were passed to a program
  * @argv: arguments vector
  *
- * Return: on scuccess, always EXIT_SUCCESS
+ * Return: on success, always EXIT_SUCCESS
  */
 int main(int argc, char *argv[])
 {
+	char *content;
 	stack_t *head;
 	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int line_number = 0;
 
 	if (argc != 2)
 	{
@@ -30,10 +35,19 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		content = content;
+		line_number++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, line_number, file);
+		}
+		free(line_number);
+	}
+	free_stack(stack);
 	fclose(file);
-
-	return (0);
-
+return (0);
 }
