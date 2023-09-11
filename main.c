30dd8539c31a014d5a613669d72d3c364ca70193
Scopecr <wjrcarrion@gmail.com>
2023-09-11 19:40:17 -0400

    Project rewrited hopefully it works

--- main.c
+++ main.c
@@ -1,26 +1,41 @@
-#include "main.h"
+#include "monty.h"
 
-char **op_toks = NULL;
-
+global_t var;
 /**
- * main - the entry point for Monty Interp
+ *  main -  interpreter for Monty ByteCodes files
+ *  @argc: Number of paramethers
+ *  @argv: Pointer to all the paramethers
  *
- * @argc: the count of arguments passed to the program
- * @argv: pointer to an array of char pointers to arguments
- *
- * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ *  Return: Always 0
  */
 int main(int argc, char **argv)
 {
-	FILE *script_fd = NULL;
-	int exit_code = EXIT_SUCCESS;
+	size_t line_buf_size = 0;
 
+	var.getl_info = NULL;
+	var.stack_head = NULL;
+	var.n_lines = 0;
 	if (argc != 2)
-		return (usage_error());
-	script_fd = fopen(argv[1], "r");
-	if (script_fd == NULL)
-		return (f_open_error(argv[1]));
-	exit_code = run_monty(script_fd);
-	fclose(script_fd);
-	return (exit_code);
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	var.fp_struct = fopen(argv[1], "r");
+	if (!var.fp_struct)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&var.getl_info, &line_buf_size, var.fp_struct) != EOF)
+	{
+		var.n_lines++;
+		if (line_validator(var.getl_info) == 1)
+			continue;
+		/*split_str(var.getl_info);*/
+		execute_opcode(split_str(var.getl_info));
+	}
+	free(var.getl_info);
+	handle_dlist_head(var.stack_head);
+	fclose(var.fp_struct);
+	return (EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -1,16 +1,14 @@
-#ifndef __MONTY_H__
-#define __MONTY_H__
+#ifndef MONTY_H
+#define MONTY_H
+#define  _GNU_SOURCE
 
+/* Libraries */
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <ctype.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 
-#define DELIMS "\n \r\t"
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -18,14 +16,13 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
-
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -34,24 +31,64 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/**
+ * struct global_s - funtional data for project
+ * @n_lines: lines counter
+ * @stack_head: stack head
+ * @fp_struct: file
+ * @getl_info: line content
+ * @node_data: data node number
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct global_s
+{
+	FILE *fp_struct;
+	stack_t *stack_head;
+	char *getl_info;
 
-extern stack_t **global_head;
+	int node_data;
+	unsigned int n_lines;
 
-void set_op_tok_error(int error_code);
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void global_free(void);
-#endif
+} global_t;
+
+/* global Variable */
+extern global_t var;
+
+/* execute the matched opcode function */
+void execute_opcode(char *opcode);
+
+/* handle the head of a doubly linked list */
+void handle_dlist_head(stack_t *head);
+
+/* get the opcode and check if the argument of push in an integer */
+char *split_str(char *str_to_split);
+
+/* helper functions */
+void is_digit(char *number);
+int line_validator(char *str);
+size_t dlistint_len(stack_t *h);
+
+/* opcode functions */
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
+void op_sub(stack_t **stack, unsigned int line_number);
+void op_div(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
+void op_mod(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
